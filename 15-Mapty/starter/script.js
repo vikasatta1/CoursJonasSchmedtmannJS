5a9487edf815ff09e0c242a05d142b9b89116629
Vika <Vikasatta1998@gmail.com>
2022-09-09 21:36:58 +0300

    mapty

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,25 +17,20 @@ navigator.geolocation.getCurrentPosition(function (position){
    const {latitude,longitude}= position.coords
     console.log(position,latitude,longitude)
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
 },function (){
     alert("Could not get your position")
 })
+console.log(firstName)
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
