8a3c1cb2dc6ab45245e9720fe597f9dc11c7b017
Vika <Vikasatta1998@gmail.com>
2022-09-11 21:32:47 +0300

    mapty 216

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,26 +11,36 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function (position){
 
-   const {latitude,longitude}= position.coords
-    console.log(position,latitude,longitude)
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const {latitude} = position.coords
+        const {longitude} = position.coords
+        console.log(position, latitude, longitude)
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
 
-    const coords = [latitude, longitude]
+        const coords = [latitude, longitude]
 
-    const map = L.map('map').setView(coords, 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        const map = L.map('map').setView(coords, 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        map.on('click', function (mapEvent) {
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+            form.classList.remove('hidden')
+            inputDistance.focus
+            /*const {lat, lng} = mapEvent.latlng
+            L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className:'running-popup'
+            }))
+                .setPopupContent('Workout').openPopup();*/
+        })
+    }, function () {
+        alert("Could not get your position")
+    })
 
-},function (){
-    alert("Could not get your position")
-})
-console.log(firstName)
 
